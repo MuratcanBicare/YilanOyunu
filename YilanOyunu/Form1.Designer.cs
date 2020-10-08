1322eceb228a65f9385b3c71bf0b4888fe7d5f3f
Muratcan Biçare <muratcanbicare@gmail.com>
2020-10-08 12:46:45 +0300

    Project Folders

---
+++ YilanOyunu/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace YilanOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.saha = new System.Windows.Forms.Panel();
+            this.cmsSaha = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.seviyeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiKolay = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiZor = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSkorlar = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblPuan = new System.Windows.Forms.Label();
+            this.lblAciklama = new System.Windows.Forms.Label();
+            this.lblDurdu = new System.Windows.Forms.Label();
+            this.lblOyunBitti = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnlGiris = new System.Windows.Forms.Panel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.btnOyunuBaslat = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtOyuncu = new MetroFramework.Controls.MetroTextBox();
+            this.saha.SuspendLayout();
+            this.cmsSaha.SuspendLayout();
+            this.pnlGiris.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // saha
+            // 
+            this.saha.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.saha.BackColor = System.Drawing.Color.Black;
+            this.saha.ContextMenuStrip = this.cmsSaha;
+            this.saha.Controls.Add(this.lblPuan);
+            this.saha.Controls.Add(this.lblAciklama);
+            this.saha.Controls.Add(this.lblDurdu);
+            this.saha.Controls.Add(this.lblOyunBitti);
+            this.saha.Location = new System.Drawing.Point(8, 70);
+            this.saha.Name = "saha";
+            this.saha.Size = new System.Drawing.Size(400, 400);
+            this.saha.TabIndex = 0;
+            this.saha.Visible = false;
+            this.saha.Paint += new System.Windows.Forms.PaintEventHandler(this.saha_Paint);
+            // 
+            // cmsSaha
+            // 
+            this.cmsSaha.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.seviyeToolStripMenuItem,
+            this.tsmiSkorlar});
+            this.cmsSaha.Name = "cmsSaha";
+            this.cmsSaha.Size = new System.Drawing.Size(111, 48);
+            // 
+            // seviyeToolStripMenuItem
+            // 
+            this.seviyeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiKolay,
+            this.tsmiZor});
+            this.seviyeToolStripMenuItem.Name = "seviyeToolStripMenuItem";
+            this.seviyeToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.seviyeToolStripMenuItem.Text = "Seviye";
+            // 
+            // tsmiKolay
+            // 
+            this.tsmiKolay.Name = "tsmiKolay";
+            this.tsmiKolay.Size = new System.Drawing.Size(102, 22);
+            this.tsmiKolay.Text = "kolay";
+            this.tsmiKolay.Click += new System.EventHandler(this.tsmiKolay_Click);
+            // 
+            // tsmiZor
+            // 
+            this.tsmiZor.Checked = true;
+            this.tsmiZor.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tsmiZor.Name = "tsmiZor";
+            this.tsmiZor.Size = new System.Drawing.Size(102, 22);
+            this.tsmiZor.Text = "zor";
+            this.tsmiZor.Click += new System.EventHandler(this.tsmiZor_Click);
+            // 
+            // tsmiSkorlar
+            // 
+            this.tsmiSkorlar.Name = "tsmiSkorlar";
+            this.tsmiSkorlar.Size = new System.Drawing.Size(110, 22);
+            this.tsmiSkorlar.Text = "Skorlar";
+            this.tsmiSkorlar.Click += new System.EventHandler(this.tsmiSkorlar_Click);
+            // 
+            // lblPuan
+            // 
+            this.lblPuan.BackColor = System.Drawing.Color.Transparent;
+            this.lblPuan.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPuan.ForeColor = System.Drawing.Color.Gold;
+            this.lblPuan.Location = new System.Drawing.Point(302, 0);
+            this.lblPuan.Name = "lblPuan";
+            this.lblPuan.Size = new System.Drawing.Size(98, 36);
+            this.lblPuan.TabIndex = 3;
+            this.lblPuan.Text = "000";
+            this.lblPuan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAciklama
+            // 
+            this.lblAciklama.BackColor = System.Drawing.Color.Transparent;
+            this.lblAciklama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAciklama.ForeColor = System.Drawing.Color.Lime;
+            this.lblAciklama.Location = new System.Drawing.Point(15, 271);
+            this.lblAciklama.Name = "lblAciklama";
+            this.lblAciklama.Size = new System.Drawing.Size(368, 108);
+            this.lblAciklama.TabIndex = 0;
+            this.lblAciklama.Text = "Oyuna başlamak veya durdurmak için \r\nF2 tuşuna basınız. \r\n\r\nYılanı hareket ettirm" +
+    "ek için \r\nyön tuşlarını (← ↑ → ↓) kullanınız.";
+            this.lblAciklama.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDurdu
+            // 
+            this.lblDurdu.BackColor = System.Drawing.Color.Transparent;
+            this.lblDurdu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurdu.ForeColor = System.Drawing.Color.Lime;
+            this.lblDurdu.Location = new System.Drawing.Point(15, 156);
+            this.lblDurdu.Name = "lblDurdu";
+            this.lblDurdu.Size = new System.Drawing.Size(368, 108);
+            this.lblDurdu.TabIndex = 1;
+            this.lblDurdu.Text = "OYUN DURDU\r\n(Devam için F2)";
+            this.lblDurdu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblDurdu.Visible = false;
+            // 
+            // lblOyunBitti
+            // 
+            this.lblOyunBitti.BackColor = System.Drawing.Color.Transparent;
+            this.lblOyunBitti.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOyunBitti.ForeColor = System.Drawing.Color.Red;
+            this.lblOyunBitti.Location = new System.Drawing.Point(15, 95);
+            this.lblOyunBitti.Name = "lblOyunBitti";
+            this.lblOyunBitti.Size = new System.Drawing.Size(368, 216);
+            this.lblOyunBitti.TabIndex = 2;
+            this.lblOyunBitti.Text = "OYUN BİTTİ\r\n(Yeni Oyun İçin F2)";
+            this.lblOyunBitti.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblOyunBitti.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pnlGiris
+            // 
+            this.pnlGiris.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlGiris.Controls.Add(this.metroLabel1);
+            this.pnlGiris.Controls.Add(this.btnOyunuBaslat);
+            this.pnlGiris.Controls.Add(this.pictureBox1);
+            this.pnlGiris.Controls.Add(this.txtOyuncu);
+            this.pnlGiris.Location = new System.Drawing.Point(8, 70);
+            this.pnlGiris.Name = "pnlGiris";
+            this.pnlGiris.Size = new System.Drawing.Size(400, 400);
+            this.pnlGiris.TabIndex = 1;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(131, 210);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(48, 19);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "Adınız:";
+            // 
+            // btnOyunuBaslat
+            // 
+            this.btnOyunuBaslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOyunuBaslat.Location = new System.Drawing.Point(131, 260);
+            this.btnOyunuBaslat.Name = "btnOyunuBaslat";
+            this.btnOyunuBaslat.Size = new System.Drawing.Size(129, 23);
+            this.btnOyunuBaslat.TabIndex = 2;
+            this.btnOyunuBaslat.Text = "Oyunu Başlat";
+            this.btnOyunuBaslat.UseVisualStyleBackColor = true;
+            this.btnOyunuBaslat.Click += new System.EventHandler(this.btnOyunuBaslat_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::YilanOyunu.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(131, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 157);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtOyuncu
+            // 
+            // 
+            // 
+            // 
+            this.txtOyuncu.CustomButton.Image = null;
+            this.txtOyuncu.CustomButton.Location = new System.Drawing.Point(107, 1);
+            this.txtOyuncu.CustomButton.Name = "";
+            this.txtOyuncu.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtOyuncu.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtOyuncu.CustomButton.TabIndex = 1;
+            this.txtOyuncu.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtOyuncu.CustomButton.UseSelectable = true;
+            this.txtOyuncu.CustomButton.Visible = false;
+            this.txtOyuncu.Lines = new string[0];
+            this.txtOyuncu.Location = new System.Drawing.Point(131, 231);
+            this.txtOyuncu.MaxLength = 32767;
+            this.txtOyuncu.Name = "txtOyuncu";
+            this.txtOyuncu.PasswordChar = '\0';
+            this.txtOyuncu.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtOyuncu.SelectedText = "";
+            this.txtOyuncu.SelectionLength = 0;
+            this.txtOyuncu.SelectionStart = 0;
+            this.txtOyuncu.ShortcutsEnabled = true;
+            this.txtOyuncu.Size = new System.Drawing.Size(129, 23);
+            this.txtOyuncu.TabIndex = 0;
+            this.txtOyuncu.UseSelectable = true;
+            this.txtOyuncu.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtOyuncu.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnOyunuBaslat;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 493);
+            this.Controls.Add(this.pnlGiris);
+            this.Controls.Add(this.saha);
+            this.MinimumSize = new System.Drawing.Size(420, 493);
+            this.Name = "Form1";
+            this.Text = "Yılan Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.saha.ResumeLayout(false);
+            this.cmsSaha.ResumeLayout(false);
+            this.pnlGiris.ResumeLayout(false);
+            this.pnlGiris.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel saha;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblAciklama;
+        private System.Windows.Forms.Label lblDurdu;
+        private System.Windows.Forms.Label lblOyunBitti;
+        private System.Windows.Forms.Label lblPuan;
+        private System.Windows.Forms.ContextMenuStrip cmsSaha;
+        private System.Windows.Forms.ToolStripMenuItem seviyeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiKolay;
+        private System.Windows.Forms.ToolStripMenuItem tsmiZor;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSkorlar;
+        private System.Windows.Forms.Panel pnlGiris;
+        private MetroFramework.Controls.MetroTextBox txtOyuncu;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.Button btnOyunuBaslat;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ YilanOyunu/SkorlarForm.cs
@@ -0,0 +1,62 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YilanOyunu
+{
+    public partial class SkorlarForm : MetroForm
+    {
+        public SkorlarForm()
+        {
+            InitializeComponent();
+            SkorlariOku();
+            //ListViewItem lvi = new ListViewItem("1");
+            //lvi.SubItems.Add("Muratcan");
+            //lvi.SubItems.Add("388");
+            //lvwSkorlar.Items.Add(lvi);
+
+        }
+
+        private void SkorlariOku()
+        {
+            try
+            {
+            string[] satirlar = File.ReadAllLines("skorlar.txt");
+            var sira = 1;
+                foreach (var satir in satirlar)
+                {
+                    string[] hucreler = satir.Split(';');
+                    int puan = Convert.ToInt32(hucreler[0]);
+                    DateTime zaman = DateTime.Parse(hucreler[1]);
+                    string ad = hucreler[2];
+                    //lvwSkorlar.Items.Add(new ListViewItem(new string[] { sira.ToString(), ad,
+                    //    puan.ToString("0000")}));
+                    SkorlariEkle(sira, ad, puan, zaman);
+                    sira++;
+                }
+            }
+            catch (Exception)
+            {
+
+                
+            }
+            
+        }
+
+        private void SkorlariEkle(int sira, string ad, int puan, DateTime zaman)
+        {
+            ListViewItem lvi = new ListViewItem(sira + ". " + ad);
+            lvi.SubItems.Add(puan.ToString("0000"));
+            lvi.SubItems.Add(zaman.ToString());
+            lvwSkorlar.Items.Add(lvi);
+        }
+    }
+}
